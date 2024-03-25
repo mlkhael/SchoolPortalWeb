5c57e13dbcaa3ba554f54ddd2968f15343ed7def
Mikhael Tabimina <mikhaelltabimina@gmail.com>
2024-03-26 01:13:36 +0800

    Add project files.

---
+++ SchoolPortal.Models/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolPortal.Models
+{
+    public class ApplicationUser
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public int YearEnrolled { get; set; }
+        [NotMapped]
+        public string Role { get; set; }
+    }
+}
---
+++ SchoolPortal.Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SchoolPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
